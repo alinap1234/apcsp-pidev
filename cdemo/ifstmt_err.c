a701b226a988a3e1966e983ea030538ef0f9d0dd
alinap1234 <alina.pollner@gmail.com>
2018-05-29 20:36:25 +0000

    working

---
+++ cdemo/ifstmt_err.c
@@ -0,0 +1,65 @@
+
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+  int b = 1;
+  if (a = 0)
+  {
+    printf("a is 0\n");
+  }
+  else
+  {
+    printf("a is not 0\n");
+  }
+
+  if (a == b)
+  { 
+    printf("a is equal to b\n");
+  }
+  if (a != b) 
+  { 
+    printf("a is not equal to b\n");
+  } 
+  if (a > b) 
+  { 
+    printf("a is greater than b\n");
+  } 
+  else
+  {
+    printf("a is less than b\n");
+  }
+  if (a >= b) 
+  { 
+    printf("a is greater than or equal to b\n");
+  } 
+  else 
+  {
+    printf("a is less than or equal to b\n");
+  }
+ if (a == 0 && b == 0) 
+  { 
+    printf("a and b are both 0\n");
+  } 
+  else 
+  {
+    printf("both a and b are not 0\n");
+  }
+ if (a == 0 || b == 0) 
+  { 
+    printf("a or b is equal to 0\n");
+  } 
+  else 
+  {
+    printf("a or b are not equal to 0\n");
+  }
+ if ( !(a == 0) ) 
+  { 
+    printf("a is not equal to 0 b\n");
+  } 
+  else 
+  {
+    printf("a is equal to 0\n");
+  }
+}
