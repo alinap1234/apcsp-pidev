4c4e2766db0ef94880fa1814112328be8488b270
alinap1234 <alina.pollner@gmail.com>
2018-05-30 21:31:11 +0000

    wooooooooooooooo

--- cdemo/stringlib.c
+++ cdemo/stringlib.c
@@ -8,9 +8,8 @@ int main()
 {
 
   char c = 97;
-  char str1[26];
+  char str1[27];
   char str3[54];
-  char str4[100];
   for (int i = 0; i < 26; i++)
   {
 	str1[i] = c;
@@ -28,10 +27,17 @@ printf("string one is %s and string two is %s\n", str1, str2);
     printf("the strings are different\n");
 
 
-for( int i = 0; i < 27; i++) 
+for( int i = 0; i < 26; i++) 
         {
-          str1[i]= str1[i] - 32;
-        }
+        c = str1[i];
+	str1[i]= c - 32;
+	printf( "%c", str1[i]);
+	 }
+
+printf("\n");
+ strcpy(str3, str1);
+ strcat(str3, str2);
+printf("concatted is %s\n", str3);
 }
 
 
