a7b6ce0a995c2119088a53b62765ad4bb30a504a
Alina Pollner <alina.pollner@gmail.com>
2018-06-01 01:30:38 +0100

    DONE

--- cdemo/userradius.c
+++ cdemo/userradius.c
@@ -3,7 +3,6 @@
 int main(int argc, char* argv[])
 {
 float areaofcircle;
-char input[256];
 int arg1;
 int arg2;
 printf("Finding the area of the circle\n");
@@ -14,27 +13,28 @@ float areaOfCircle(float a)
   return 3.14*a*a;
 }
 
-if(argc ==3);
+if(argc ==3)
 {
 
 int arg1;
 
-if(sscanf(argv[1], "%d", &arg1) == 1)
-{
-	
+	if(sscanf(argv[1], "%d", &arg1) == 1)
+	{
 	sscanf(argv[1],"%d", &arg1);
-}
-	else {printf("not an integer");}
+	printf("Yay an integer!\n");
+	}
+	else {printf("First input is not an integer\n");
+	return 1;}
 
 int arg2;
 
-if(sscanf(argv[2], "%d", &arg2) == 1)
-{
-	
-      sscanf(argv[2],"%d", &arg2);
-}
-
-	else {printf("not an integer");}
+	if(sscanf(argv[2], "%d", &arg2) == 1)
+	{
+	sscanf(argv[2],"%d", &arg2);
+	printf("Yay an integer!\n");
+	}
+	else {printf("Second input is not an integer\n");
+	return 1;}
 
 
 printf("lower-bound radius %d and upper-boud radius %d\n", arg1, arg2);
@@ -48,5 +48,35 @@ float r;
 
 }
 
-else   { printf("we're gonna need to ask for some value"
+else {
+printf("we're gonna need to ask for some values\n");
+
+int num;
+char input[256];
+char input2[256];
+int num2;
+float r;
+
+fgets(input, 256, stdin);
+ 
+ if (sscanf(input, "%d", &num) != 1)
+    {
+	printf("Not a valid integer\n");
+	return 1;
+    } 
+fgets(input2, 256, stdin);  
+
+if (sscanf(input2, "%d", &num2) != 1)
+    {
+	printf("Not a valid integer - try again!\n"); 
+	return 1;
+    }
+
+ for (int i = num; i <= num2; i++)
+    {
+      r = areaOfCircle(i);
+      printf("Radius = %d, and Area = %f\n", i, r);
+    } 
+
+}
 }
